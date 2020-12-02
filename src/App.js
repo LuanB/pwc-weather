2541ce3eab02a724d2413dd6c95d91a6b0dd8f7a
Luan Bui <luanb3455@gmail.com>
2020-12-02 15:06:53 +0800

    wapping wether widget into provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import WeatherWidgetContainer from "./components/WeatherWidgetContainer";
+import { AppContextProvider } from "./context/appContext";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <WeatherWidgetContainer />
+      <AppContextProvider>
+        <WeatherWidgetContainer />
+      </AppContextProvider>
     </div>
   );
 }
