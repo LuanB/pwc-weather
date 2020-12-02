c1984dc7ba98d0dfb2279d2966ee3777caaae4d4
Luan Bui <luanb3455@gmail.com>
2020-12-02 23:49:44 +0800

    search feature with hooks and context

--- src/components/SearchContainer/index.jsx
+++ src/components/SearchContainer/index.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { DispatchContext } from "../../context/appContext";
+import { useInputState } from "./hooks/useInputState";
+import TextField from "@material-ui/core/TextField";
 import "./SearchContainerStyles.css";
 
 export default function SearchContainer() {
-  return <div>Search Container component</div>;
+  const [value, handleChange, reset] = useInputState("");
+  const dispatch = useContext(DispatchContext);
+  return (
+    <div className="SearchContainer">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          dispatch({ type: "SetCity", payload: value });
+          reset();
+        }}
+      >
+        <TextField
+          value={value}
+          onChange={handleChange}
+          margin="normal"
+          label="Search City Weather"
+          fullWidth
+        />
+      </form>
+    </div>
+  );
 }
